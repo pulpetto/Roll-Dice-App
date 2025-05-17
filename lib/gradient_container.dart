164e92423d840a23c712ae5ce5d3563ffaad4f4c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-17 13:23:45 +0200

    add method

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
+
+  void rollDice() {}
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,15 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(child: StyledText('Hello')),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/dice-1.png', width: 200),
+            TextButton(onPressed: rollDice, child: Text('Roll Dice')),
+          ],
+        ),
+      ),
     );
   }
 }
