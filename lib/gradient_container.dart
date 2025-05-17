f98570b7598d69ac435bd74d106347914851de2f
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-05-17 08:25:29 +0200

    refactor into separate files

---
+++ lib/gradient_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 22, 2, 56),
+            Color.fromARGB(255, 58, 5, 148),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(child: StyledText()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 22, 2, 56),
-                Color.fromARGB(255, 58, 5, 148),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.white, fontSize: 28.0),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
